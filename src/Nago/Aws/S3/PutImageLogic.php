34f574c345388550672bbbc6b42cd81c53b6b8f3
Nagomi <nagomi.github@gmail.com>
2018-09-11 05:49:18 +0900

    Add PutImageLogic

---
+++ src/Nago/Aws/S3/PutImageLogic.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nagomi
+ * Date: 2018/09/11
+ * Time: 5:28
+ */
+
+class PutImageLogic
+{
+	protected $s3;
+	protected $bucket;
+	protected $url;
+	public function __construct(\Aws\S3\S3Client $s3, $bucket) {
+		$this->s3 = $s3;
+		$this->bucket = $bucket;
+		$this->url = '';
+	}
+
+	/**
+	 * @param $src_filename
+	 * @param $dest_filename
+	 * @return $this
+	 * @throw \Aws\S3\Exception\S3Exception
+	 */
+	public function run(string $src_filename, string $dest_filename){
+		try {
+			$content_type = mime_content_type($src_filename);
+			/* @var \Aws\Result $awsResult */
+			$awsResult = $this->s3->putObject([
+				'Bucket' => $this->bucket,
+				'Key'    => $dest_filename,
+				'Body'   => fopen($src_filename, 'r'),
+				'ACL'    => 'public-read',
+				'ContentType' => $content_type,
+			]);
+		} catch (\Aws\S3\Exception\S3Exception $e) {
+			throw $e;
+		}
+		$this->url = $awsResult['ObjectURL'];
+		return $this;
+	}
+	public function getImageUrl() : string{
+		return $this->url;
+	}
+}
